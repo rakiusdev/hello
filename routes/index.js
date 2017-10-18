eca2ddc657484f7ccc8641097c3ffe3e50fbcdfb
Rakius <rakius.it@gmail.com>
2017-10-18 12:30:30 +0200

    Just copying pasting

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var models = require("../models");
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  models.sequelize
+    .query(
+      `SELECT
+          name,
+          birth_date,
+          EXTRACT(YEAR FROM age(birth_date)) AS age
+      FROM
+          "People"
+      ORDER BY
+          birth_date DESC`,
+      {
+        model: models.Person
+      }
+    )
+    .then(function(people) {
+      res.render("index", { title: "Celebrities, ordered by age", people: people });
+      console.log("First person: " + people[0].name);
+    });
+});
+
+module.exports = router;
